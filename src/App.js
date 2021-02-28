71fb8391df50805ff88e687e859089b1056920e8
Sample Tech <sample.tech29@gmail.com>
2021-02-28 22:45:24 +0530

    Adding Ecom

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './App.css';
+import Header from "./Header.js";
+import Home from "./Home.js";
+
+
+function App() {
+  return (
+    <div className="app">
+      
+      <Header/>
+      <Home/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBveYVE8UFoorxN-AqcqiUF4EG1Mjd11pE",
+  authDomain: "ecom-d4b0e.firebaseapp.com",
+  projectId: "ecom-d4b0e",
+  storageBucket: "ecom-d4b0e.appspot.com",
+  messagingSenderId: "853708551259",
+  appId: "1:853708551259:web:28d03bd3eb61e6f15bbb7e",
+  measurementId: "G-XWP4H0EZTE"
+};
